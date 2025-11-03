fa22860b6352897ddecd0b0b0a6eef7396b86167
Matthew Tindogan <77546248+TrainerBlu3@users.noreply.github.com>
2025-11-03 10:30:29 -0800

    Correct syntax and enhance member management methods
    Fixed syntax errors and improved member management methods.

--- library/Library.java
+++ library/Library.java
@@ -1,15 +1,37 @@
 package lab4.library;
 
+imprt java.util.ArrayList;
+
 public class Library {
 	
 	//  GRASP Principles: Creator, Controller
 	//	Manage the catalog of books and members.
 	//	Track which books are borrowed and available
-	
+	private List<Book> memberBook;
+	private List<Member> memberList;
+
+	public Library(memberBook, memberList){
+		this.memberBook = memberBook;
+		this.memberList = memberList;
+	}
 	
 	// Attributes:
 	// private catalog (list of Book) 
 	// private members (list of Members)
+
+	private void catalog(){
+		System.out.println("List of Books: ");
+		for(String book : memberBook){
+			System.out.println(book);
+		}
+	}
+
+	private void members(){
+		System.our.println("List of Members: ");
+		for(String individualMember : memberList){
+			System.out.println(individualMember);
+		}
+	}
 
 	// TODO: implement functionality of Member class
 
