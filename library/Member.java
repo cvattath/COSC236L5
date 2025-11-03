ade36c9173dc386db53fe37c73afc16bbb4262f1
Christy Vattatharakooran Kunjachan <christyvk.chris@gmail.com>
2025-11-02 22:28:34 -0800

    Update Member.java

--- library/Member.java
+++ library/Member.java
@@ -1,4 +1,5 @@
 package lab4.library;
+import.java.util.ArrayList;
 
 public class Member {
 
@@ -6,8 +7,26 @@ public class Member {
 	// Can borrow and return books.
 
 	private String name;
-	// private borrowedbBooks TODO: implement collection of borrowed books
+	private int memberId;
+	
+	private List<Book> borrowedBooks;
+	TODO: implement collection of borrowed books
 	
 	// TODO: implement functionality of Member class
+	
+	public Member(String name, int memeberId) {
+		this.name = name;
+		this.memberId = memberId;
+		borrowedBooks = new ArrayList<>();
+	}
+	
+	public boolean borrowBook(Book book) {
+		if(book.isAvailable()) {
+			book.setAvailable(false);
+		}
+			borrowedBooks.add(book);
+			return true;
+		}
+	}
 
 }
